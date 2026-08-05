02850ef12024d33d716f146b0ce8eb5830dcbf13
Midhush Kanna <kannamidhush@gmail.com>
2026-08-05 16:44:58 +0530

    Add Firebase authentication

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2qHbKHUq0PVwNCSKCwwqfQygQSsCYFmk",
-  authDomain: "edunext-learning-platform.firebaseapp.com",
-  projectId: "edunext-learning-platform",
-  storageBucket: "edunext-learning-platform.firebasestorage.app",
-  messagingSenderId: "82274048841",
-  appId: "1:82274048841:web:1ed72dff9c7b7fc2a39898",
-  measurementId: "G-2KJ8NYRBBV",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
